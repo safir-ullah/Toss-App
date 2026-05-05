4a7f769394a0218075a39288b8240808a03f24e0
safir-ullah <safirullahakbar@gmail.com>
2026-05-05 22:58:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function tossCoin() {
+
+    let coin = document.getElementById("coin");
+    let result = document.getElementById("result");
+
+    coin.style.transform = "rotateY(720deg)";
+
+    setTimeout(function () {
+
+        let random = Math.ceil(Math.random() * 2);
+
+       if (random == 1) {
+    coin.src = "Head.png";
+    result.innerHTML = "Head";
+}
+else {
+    coin.src = "Tail.png";
+    result.innerHTML = "Tail";
+}
+
+        coin.style.transform = "rotateY(0deg)";
+
+    }, 1000);
+
+}
